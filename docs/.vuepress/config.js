f66c9d604eefea696bad7e79065472e28a382849
dreamingfan1 <308790039@qq.com>
2020-04-23 21:42:56 +0800

    首次提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: '朱小通',
+    description: '欢迎来到我的主页',
+    evergreen: true,
+    themeConfig: {
+        nav: [
+          { text: '首页', link: '/' },
+          { text: '博客', link: '/guide/' },
+        ]
+    }
+}
