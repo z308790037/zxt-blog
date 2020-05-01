cbd813f887d3efe188ac656a769bc1a691d8a172
dreamingfan1 <308790039@qq.com>
2020-05-01 10:58:03 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,37 @@
 module.exports = {
     title: '朱小通',
-    description: '欢迎来到我的主页',
+    description: '欢迎来到我的主页，这是我的地盘，但也希望留下您的脚印~',
     evergreen: true,
+    base: 'zxt-blog',
     themeConfig: {
+        lastUpdated: 'Last Updated', 
+        smoothScroll: true,
         nav: [
-          { text: '首页', link: '/' },
-          { text: '博客', link: '/guide/' },
-        ]
+          { text: '主页', link: '/' },
+          {
+            text: '技术笔记',
+            ariaLabel: 'notes',
+            items: [
+              { text: '前端笔记', link: '/notes/front/' },
+              { text: 'node笔记', link: '/notes/node/' },
+              { text: 'java笔记', link: '/notes/java/' },
+              { text: 'python笔记', link: '/notes/python/' },
+            ]
+          },
+          { text: '技术分享', link: '/share/'},
+          { text: '查看源码', link: 'https://github.com/z308790037/zxt-blog' },
+        ],
+        sidebar: {
+          '/notes/front/': [{
+            title: 'js基础',
+            children:['', 'js_one', 'js_two']
+          },{
+            title: '框架相关',
+            children:['frame']
+          },{
+            title: '其他',
+            children:['other']
+          }]
+        },
     }
 }
--- docs/README.md
+++ docs/README.md
@@ -2,11 +2,13 @@
 home: true
 actionText: zxt的博客 →
 actionLink: /guide/
+search: true
 features:
-- title: 简洁
-  details: 以 Markdown 中心的项目结构，以最少的配置帮助你专注于写作。
-- title: Vue驱动
-  details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-- title: 高性能
-  details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
+- title: 个人介绍
+  details: 热衷于前端开发， 专注于前端开发，不局限于前端开发
+- title: 开发笔记
+  details: 前端学习笔记，node学习笔记，python学习笔记，java学习笔记
+- title: 技术分享
+  details: 收集大神们的一些经典博客，记录一些自己的心得体会
+
 ---
